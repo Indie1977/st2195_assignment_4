6e393e55be8e3582028dfcacf669e0b8786d3c78
Chris C <caffrey.chris@gmail.com>
2022-03-29 02:39:13 +0100

    Second Commit

--- foo.R
+++ foo.R
@@ -4,17 +4,27 @@
 # Radius
 r <- 2
 # Function to compute the volume of a sphere with radius r
-volume <- function(r, rho) {
-  3/4*pi*r^2
+volume <- function(r) {
+  # Changed 3/4 to 4/3
+  # Changed r^2 to r^3
+  4/3*pi*(r^3)
 }
-# Function to compute the volumes of the spheres with radius r, r^2 and
-r^3
+volume(r)
+
+
+
+# Function to compute the volumes of the spheres with radius r, r^2 and r^3
+  # Brought last work of text up to markdown line
+  # Took r outside of function
+  # Changed "r" in the for loop to "i"
+r <- 22
 volume_vector <- function(r) {
-  r <- 22
-  for (r in 2:4){
-    volume(r)
+  for (i in 1:3){
+    volume(r^i)
   }
 }
-# Run volume_vector(r) and print the volumes of the spheres with radius
-r, r^2 and r^3
+
+
+# Run volume_vector(r) and print the volumes of the spheres with radius r, r^2 and r^3
 volume_vector(r)
+
